99dadf5f31388d4c6fcb4aed3bd459b245e7e127
思维空间 <dnyz520@126.com>
2020-04-10 16:53:20 +0800

    up

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'stats.js',
     // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
-    library: 'StatsCs',
+    library: 'statsCS',
     // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
     libraryTarget: 'umd',
     // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
